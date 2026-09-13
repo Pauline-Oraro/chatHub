6aecd8fab75251dd6460346fc1bbce48358b5eae
Pauline-Oraro <hiltrapauline@gmail.com>
2026-09-13 18:40:44 +0300

    database connectivity and added middleware for JSON parsing, CORS, and Clerk authentication

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,20 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config"
+import {clerkMiddleware} from "@clerk/express";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}!!!`));
+//middlewares
+app.use(express.json());
+
+app.use(cors({ origin: FRONTEND_URL, credentials:true }));
+
+app.use(clerkMiddleware());
+
+app.listen(process.env.PORT, () => {
+    connectDB();
+    console.log(`Server running on port ${process.env.PORT}!!!`);
+});
