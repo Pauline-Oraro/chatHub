b6a7078cb6d25c099f0f47ca5186993cd577492b
Pauline-Oraro <hiltrapauline@gmail.com>
2026-09-14 16:37:01 +0300

    wrap App component with ClerkProvider for authentication context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 
 import './App.css'
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
 
 function App() {
 
 
   return (
     <div>
+      <header>
+        <Show when="signed-out">
+          <SignInButton mode='modal' />
+          <SignUpButton mode='modal' />
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
       <h1>CHATHUB ~ A real-time chat application</h1>
     </div>
   )
