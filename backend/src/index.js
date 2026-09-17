10851d14105c3cba2560f66ed6e86cf4907f2830
Pauline-Oraro <hiltrapauline@gmail.com>
2026-09-17 17:49:20 +0300

    add clerk webhook handler and integrate with API

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,14 @@ import path from "path";
 import {clerkMiddleware} from "@clerk/express";
 import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
+import clerkWebhook from "./webhooks/clerk.webhook.js";
 
 const app = express();
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
+
+app.use("/api/webhooks/clerk", express.raw({ type: "application/json" }), clerkWebhook);
 
 //middlewares
 app.use(express.json());
