21252a83bee4c0b3cdb4da307226ac714609b941
Pauline-Oraro <hiltrapauline@gmail.com>
2026-09-17 12:33:18 +0300

    organize imports and enhance static file serving in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config"
+import "dotenv/config";
+import fs from "fs";
+import path from "path";
 import {clerkMiddleware} from "@clerk/express";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public");
 
 //middlewares
 app.use(express.json());
@@ -13,6 +17,14 @@ app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials:true }));
 
 app.use(clerkMiddleware());
+
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 app.listen(process.env.PORT, () => {
     connectDB();
